2edf80e5655f830131fba66d9067b84ab994ac3c
AhmedRK7 <46600282+AhmedRK7@users.noreply.github.com>
2019-02-02 20:19:51 +0400

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,42 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+
+client.login('NTQxMjgxMTIxMDU3NTA1MzA1.DzdK9w.VMnNnu3yDsThvajD8y8kefz1O-w'); 
+
+
+
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('by Ahmed R K');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('is online')
+client.user.setStatus("dnd");
+});
+
+// playing
+client.on('ready', () => {                           
+client.user.setGame(`ROYALE GAMES`,'by AHMED RK');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(`-help | On 20 Servers`,'https://www.twitch.tv/v5bz');
+});          
+
+
+
+
+
+
+
+
+client.on('message', message => {
+     if (message.content === "") {
+message.author.send("**اوامر البوت**" + `  **
+
+ 
+
+**`);
+    }
+});     
+                                                                                                                                      
